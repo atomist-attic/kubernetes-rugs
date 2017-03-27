88255842f9d6813eefbb07d6ebd44ca985982805
Rod Johnson <rod@atomist.com>
2017-03-27 16:52:34 +0100

    Renamed file

---
+++ .atomist/handlers/event/Deployed.ts
@@ -0,0 +1,35 @@
+import { HandleEvent, Message, Plan } from '@atomist/rug/operations/Handlers'
+import { GraphNode, Match, PathExpression } from '@atomist/rug/tree/PathExpression'
+import { EventHandler, Tags } from '@atomist/rug/operations/Decorators'
+import * as query from '@atomist/rugs/util/tree/QueryByExample'
+import { Pod } from "@atomist/cortex/stub/Pod"
+import { Container } from "@atomist/cortex/stub/Container"
+import { Tag } from "@atomist/cortex/stub/Tag"
+
+/*
+new PathExpression<GraphNode, GraphNode>(
+    `/Pod()[@state="Started"]
+        [/uses::Container()/isTagged::Tag()]`)
+        */
+
+@EventHandler("pod-deployed", "Handle Kubernetes Pod deployment events", 
+     query.forRoot(
+         new Pod().withState("Started")
+            .withUses(new Container()
+                .withIsTagged(new Tag())
+)))
+@Tags("kubernetes")
+class Deployed implements HandleEvent<Pod, GraphNode> {
+    handle(event: Match<Pod, GraphNode>): Plan {
+        let pod: Pod = event.root()
+
+        let plan: Plan = new Plan()
+
+        let message: Message = new Message("deployed")
+        plan.add(message)
+
+        return plan
+    }
+}
+
+export const deployed = new Deployed()
