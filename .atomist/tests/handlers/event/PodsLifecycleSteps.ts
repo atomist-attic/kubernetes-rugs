6283fdd9c5c4a457813b3f82e8c780f856cd00fb
David Dooling <david@atomist.com>
2017-04-24 13:29:44 -0500

    Fix TypeScript compile/lint issues
    I am not sure why the build and test worked locally.  Perhaps
    differing tsc versions.

--- .atomist/tests/handlers/event/PodsLifecycleSteps.ts
+++ .atomist/tests/handlers/event/PodsLifecycleSteps.ts
@@ -1,121 +1,126 @@
-import { Given, When, Then, EventHandlerScenarioWorld } from "@atomist/rug/test/handler/Core"
-import { DirectedMessage, LifecycleMessage, PlanMessage } from "@atomist/rug/operations/Handlers"
-import { Build } from "@atomist/cortex/stub/Build"
-import { Commit } from "@atomist/cortex/stub/Commit"
-import { Tag } from "@atomist/cortex/stub/Tag"
-import { DockerImage } from "@atomist/cortex/stub/DockerImage"
-import { K8Pod } from "@atomist/cortex/stub/K8Pod"
-import { K8Spec } from "@atomist/cortex/stub/K8Spec"
-import { Environment } from "@atomist/cortex/stub/Environment"
-import { Person } from "@atomist/cortex/stub/Person"
-import { ChatId } from "@atomist/cortex/stub/ChatId"
-import { GitHubId } from "@atomist/cortex/stub/GitHubId"
-import { Repo } from "@atomist/cortex/stub/Repo"
-import { Push } from "@atomist/cortex/stub/Push"
-import { ChatChannel } from "@atomist/cortex/stub/ChatChannel"
+/*
+ * Copyright © 2017 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+import { EventHandlerScenarioWorld, Given, Then, When } from "@atomist/rug/test/handler/Core";
 
-function buildPodEvent(state: string, domain: string = "prod.atomist.services."): K8Pod {
-    const chatId: ChatId = new ChatId
-    chatId.withScreenName("me")
-   
-    const person: Person = new Person
-    person.withChatId(chatId)
+import { DirectedMessage, LifecycleMessage, PlanMessage } from "@atomist/rug/operations/Handlers";
 
-    const gitHubId: GitHubId = new GitHubId
-    gitHubId.withPerson(person)
+import * as stub from "@atomist/cortex/stub/Types";
 
-    const commit: Commit = new Commit
-    const tag: Tag = new Tag
-    commit.withAuthor(gitHubId)
-    tag.withCommit(commit)
-    tag.withName("sometag")
-    commit.addTags(tag)
-    commit.withSha("b20479edc0c2a202b18814cbd4e95463df04fb83")
+function buildPodEvent(state: string, domain: string = "prod.atomist.services."): stub.K8Pod {
+    const chatId = new stub.ChatId();
+    chatId.withScreenName("me");
 
-    const build: Build = new Build
-    commit.addBuilds(build)
-    build.withCommit(commit)
+    const person = new stub.Person();
+    person.withChatId(chatId);
 
-    const push: Push = new Push
-    push.addCommits(commit)
-    push.addBuilds(build)
-    build.withPush(push)
-    commit.withPush(push)
+    const gitHubId = new stub.GitHubId();
+    gitHubId.withPerson(person);
 
-    const repo: Repo = new Repo
-    commit.withRepo(repo)
-    push.withRepo(repo)
-    repo.withOwner("atomist-rugs")
-    repo.withName("myrugs")
+    const commit = new stub.Commit();
+    const tag = new stub.Tag();
+    commit.withAuthor(gitHubId);
+    tag.withCommit(commit);
+    tag.withName("sometag");
+    commit.addTags(tag);
+    commit.withSha("b20479edc0c2a202b18814cbd4e95463df04fb83");
 
-    const channel: ChatChannel = new ChatChannel
-    repo.addChannels(channel)
+    const build = new stub.Build();
+    commit.addBuilds(build);
+    build.withCommit(commit);
 
-    const image: DockerImage = new DockerImage
-    image.withTag(tag)
+    const push = new stub.Push();
+    push.addCommits(commit);
+    push.addBuilds(build);
+    build.withPush(push);
+    commit.withPush(push);
 
-    const environment: Environment = new Environment
-    environment.withName(domain)
+    const repo = new stub.Repo();
+    commit.withRepo(repo);
+    push.withRepo(repo);
+    repo.withOwner("atomist-rugs");
+    repo.withName("myrugs");
 
-    const spec: K8Spec = new K8Spec
-    spec.withEnvironment(environment)
-    image.withSpec(spec)
-    spec.addImages(image)
+    const channel = new stub.ChatChannel();
+    repo.addChannels(channel);
 
-    const pod: K8Pod = new K8Pod
-    pod.withState(state)
-    pod.withSpec(spec)
-    pod.addImages(image)
-    pod.withName("myservice")
+    const image = new stub.DockerImage();
+    image.withTag(tag);
 
-    return pod
+    const environment = new stub.Environment();
+    environment.withName(domain);
+
+    const spec = new stub.K8Spec();
+    spec.withEnvironment(environment);
+    image.withSpec(spec);
+    spec.addImages(image);
+
+    const pod = new stub.K8Pod();
+    pod.withState(state);
+    pod.withSpec(spec);
+    pod.addImages(image);
+    pod.withName("myservice");
+
+    return pod;
 }
 
 Given("pod event handler registered", (world: EventHandlerScenarioWorld) => {
-    world.registerHandler("pod-event")  
-})
+    world.registerHandler("pod-event");
+});
 
 When("crash looping occurs", (world: EventHandlerScenarioWorld) => {
-    const pod: K8Pod = buildPodEvent("BackOff")
-    world.sendEvent(pod)
-})
+    const pod = buildPodEvent("BackOff");
+    world.sendEvent(pod);
+});
 
 When("a deployment was successful", (world: EventHandlerScenarioWorld) => {
-    const pod: K8Pod = buildPodEvent("Started")
-    world.sendEvent(pod)
-})
+    const pod = buildPodEvent("Started");
+    world.sendEvent(pod);
+});
 
 When("a container image is pulling", (world: EventHandlerScenarioWorld) => {
-    const pod: K8Pod = buildPodEvent("Pulling")
-    world.sendEvent(pod)
-})
+    const pod = buildPodEvent("Pulling");
+    world.sendEvent(pod);
+});
 
 When("a pod is terminating", (world: EventHandlerScenarioWorld) => {
-    const pod: K8Pod = buildPodEvent("Killing")
-    world.sendEvent(pod)
-})
+    const pod = buildPodEvent("Killing");
+    world.sendEvent(pod);
+});
 
 When("a pod is unhealthy", (world: EventHandlerScenarioWorld) => {
-    const pod: K8Pod = buildPodEvent("Unhealthy")
-    world.sendEvent(pod)
-})
+    const pod = buildPodEvent("Unhealthy");
+    world.sendEvent(pod);
+});
 
 Then("the handler is triggered", (world: EventHandlerScenarioWorld) => {
-    return world.plan() != null
-})
+    return world.plan() != null;
+});
 
 Then("we should not receive anything", (world: EventHandlerScenarioWorld) => {
-    return world.plan().messages.length === 0 
-})
+    return world.plan().messages.length === 0;
+});
 
 Then("the committer should receive a direct message", (world: EventHandlerScenarioWorld) => {
-    const message = world.plan().messages[1] as DirectedMessage
-    return message.usernames[0] == "me"
-})
+    const message = world.plan().messages[1] as DirectedMessage;
+    return message.usernames[0] === "me";
+});
 
 Then("we should receive a message", (world: EventHandlerScenarioWorld) => {
-    const lifecycleId = "commit_event/atomist-rugs/myrugs/b20479edc0c2a202b18814cbd4e95463df04fb83"
-    const message = world.plan().messages[0] as LifecycleMessage
-    return message.lifecycleId == lifecycleId
-})
+    const lifecycleId = "commit_event/atomist-rugs/myrugs/b20479edc0c2a202b18814cbd4e95463df04fb83";
+    const message = world.plan().messages[0] as LifecycleMessage;
+    return message.lifecycleId === lifecycleId;
+});
